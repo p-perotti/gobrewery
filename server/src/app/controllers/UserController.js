3c3d54ecb9eb29d54f72065c738ea7af0bf58a65
Patrick <patrickperotti0@gmail.com>
2020-02-11 20:29:46 -0300

    Controller de usuários

---
+++ server/src/app/controllers/UserController.js
@@ -0,0 +1,42 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(8),
+      name: Yup.string().required(),
+      administrator: Yup.boolean().required(),
+      active: Yup.boolean().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { id, email, name, administrator, active } = await User.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      email,
+      name,
+      administrator,
+      active,
+    });
+  }
+}
+
+export default new UserController();
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,19 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    email: 'patrickperotti0@gmail.com',
-    password_hash: '666',
-    name: 'Patrick Perotti',
-    type: 'F',
-    birthdate: '1996-04-01',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
