f79b139fe26b3e76936b4645beaa1854c9dcdbba
Patrick <patrickperotti0@gmail.com>
2020-05-11 04:54:26 -0300

    store configuration with redux and redux saga

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 
+import store from './store';
 import Routes from './routes';
 import history from './services/history';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
